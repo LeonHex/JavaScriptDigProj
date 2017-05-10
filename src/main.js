c7631ee5f46996e889a3593e3b3f0aad0e459ebe
guang yang <blasttnt@gmail.com>
2017-05-10 21:38:13 +0800

    scope_experiments

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
 console.log("this. is the first line of the js");
+
+function navigateToNewPage(){
+    window.open("index2.html", "_self");
+}
+
+/* ================================================
+ *   global scope and function scope
+ * ================================================
+*/
+globalFunc();
+
+// var globalFunc = function(){
+function globalFunc() {
+    test = "this is a global variable";
+    var test1 = "this is a function scope variable";
+}
+
+console.log("page1:" + window.test);
+// console.log(test1);
+
+//override the system global variable
+// console.info(addEventListener);
+// addEventListener = 333;
+// console.info(addEventListener);
+// console.info(window.addEventListener);
+
+//=================================================
+
+function thisTestFunc(arg1, arg2){
+    var result = arg1 + arg2;
+    return result;
+}
+var res = thisTestFunc(5, 10);
+
+// var thisTestVar = new thisTestFunc();
+console.info(thisTestFunc);
