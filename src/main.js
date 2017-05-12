2ff41d4379912f048600db6770a3e417cf0825b6
guang yang <blasttnt@gmail.com>
2017-05-12 16:09:41 +0800

    prototype_experiment

--- src/main.js
+++ src/main.js
@@ -39,15 +39,52 @@ console.info(thisTestFunc);
 //=================================================
 
 
-function siblings() {
-    var siblings = ["John", "Liza", "Peter"];
-    function siblingCount() {
-        var siblingsLength = siblings.length;
-        return siblingsLength;
-    }
-    function joinSiblingNames() {
-        return "I have " + siblingCount() + " siblings:\n\n" + siblings.join("\n");
-    }
-    return joinSiblingNames;
+// function siblings() {
+//     var siblings = ["John", "Liza", "Peter"];
+//     function siblingCount() {
+//         var siblingsLength = siblings.length;
+//         return siblingsLength;
+//     }
+//     function joinSiblingNames() {
+//         return "I have " + siblingCount() + " siblings:\n\n" + siblings.join("\n");
+//     }
+//     return joinSiblingNames;
+// }
+// alert(siblings()());
+
+// function addLinks() {
+//     for (var i = 0, link; i < 5; i++) {
+//         link = document.createElement("a");
+//         link.innerHTML = "Link " + i;
+//         link.onclick = function (num) {
+//             return function () {
+//                 alert(num);
+//             };
+//         }(i);
+//         document.body.appendChild(link);
+//     }
+// }
+// window.onload = addLinks;
+
+var Person = function(name){
+    this.name = name;
+    // var obj = {
+    //     name: name
+    // }
+
+    // return this;
 }
-alert(siblings()());
+
+var p1 = new Person("hex");
+console.log(p1.name);
+
+p1.age = 19;
+console.info(p1);
+
+// Person.nationality = "Chinese";
+Person.prototype.nationality = "Chinese";
+var p2 = new Person("leon");
+console.info(p2);
+
+var p3 = Person.nationality;
+console.info(p3);
